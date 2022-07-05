9679f8fc0f7455af6da74b5714e247525cb19521
begimaib <begimai.bolotbekova>
2022-07-05 18:50:12 +0600

    superuser created

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'category', 'remainder', 'price']
+    list_display_links = ['name']
+    list_filter = ['name']
+    search_fields = ['name', 'description']
+    fields = ['name', 'description', 'category', 'remainder', 'price']
+
+
+admin.site.register(Product, ProductAdmin)
+
