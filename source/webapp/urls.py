f8cc1e0bbd2305adeb53a6233ac476e182d74ced
begimaib <begimai.bolotbekova>
2022-08-02 10:01:07 +0600

    urls are changed due to class-based views

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from webapp.views import index_view, create_product, product_view, update_product, delete_product
+from webapp.views import IndexView, ProductView, CreateProduct, UpdateProduct, DeleteProduct
 
 
 urlpatterns = [
-    path('', index_view, name="index"),
-    path('products/add/', create_product, name="create"),
-    path('product/<int:pk>/', product_view, name="product_view"),
-    path('product/<int:pk>/update', update_product, name="update_product"),
-    path('product/<int:pk>/delete', delete_product, name="delete_product"),
+    path('', IndexView.as_view(), name="index"),
+    path('products/add/', CreateProduct.as_view(), name="create"),
+    path('product/<int:pk>/', ProductView.as_view(), name="product_view"),
+    path('product/<int:pk>/update', UpdateProduct.as_view(), name="update_product"),
+    path('product/<int:pk>/delete', DeleteProduct.as_view(), name="delete_product"),
 ]
