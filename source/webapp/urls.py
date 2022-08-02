1c32228aa9d91c69aaf7acd6a48dc4ec9610e2d7
begimaib <begimai.bolotbekova>
2022-08-03 00:57:38 +0600

    urls have new links due to the cart

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from webapp.views import IndexView, ProductView, CreateProduct, UpdateProduct, DeleteProduct
+from webapp.views import CartProductView, CreateCartProduct, DeleteCartProduct
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('products/cart', CartProductView.as_view(), name="cart_product_view"),
+    path('products/cart/add/<int:pk>', CreateCartProduct.as_view(), name="create_cart_product"),
+    path('products/<int:pk>/delete', DeleteCartProduct.as_view(), name="delete_cart_product"),
     path('products/add/', CreateProduct.as_view(), name="create"),
     path('product/<int:pk>/', ProductView.as_view(), name="product_view"),
     path('product/<int:pk>/update', UpdateProduct.as_view(), name="update_product"),
